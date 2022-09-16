f44990f4cafd55206bd7d450703d4ea82195efa6
koladev <onaelmangabo@gmail.com>
2022-09-16 13:24:12 +0100

    :sparkles: add HelloWorld package

---
+++ hello_word/__init__.py
@@ -0,0 +1,10 @@
+# Simple project that print hello-world
+
+class HelloWorld:
+    __version__ = "0.0.0"
+
+    def __init__(self, message):
+        self.message = message
+
+    def log(self):
+        print(self.message)
